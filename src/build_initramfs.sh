e6f991e994e9de30dc15f87c4146fa22274abe41
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-04-09 14:58:14 +0200

    build initramfs: do not hardcode package versions
    While it can be good to have those tracked, handling that should be
    less friction. (e.g., simply output them a file tracked in git)
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/build_initramfs.sh
+++ src/build_initramfs.sh
@@ -7,12 +7,12 @@ BUILDDIR="build/initramfs"
 INIT="../../init-shim-rs/target/release/init-shim-rs"
 
 PKGS=" \
-    libc6:amd64=2.28-10 \
-    libgcc1:amd64=1:8.3.0-6 \
-    libstdc++6:amd64=8.3.0-6 \
-    libssl1.1:amd64=1.1.1d-0+deb10u4 \
-    libattr1:amd64=1:2.4.48-4 \
-    libacl1:amd64=2.2.53-4
+    libc6:amd64 \
+    libgcc1:amd64 \
+    libstdc++6:amd64 \
+    libssl1.1:amd64 \
+    libattr1:amd64 \
+    libacl1:amd64
 "
 
 echo "Using build dir: $BUILDDIR"
